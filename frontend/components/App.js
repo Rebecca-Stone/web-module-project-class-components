56f3bddae283018a6f9b1c21e5437361ad6f026e
Rebecca Stone <you@example.com>
2022-03-07 15:45:06 -0600

    converted the directions to pseudo code to give self some direction

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Form from "./Form";
+import Todo from "./Todo";
+import TodoList from "./TodoList";
+
+//`<App />` will hold all the data needed for this project.
+//All of your application data will be stored here on `<App />`.
 
 export default class App extends React.Component {
+  //All of your `handler` functions should live here on `<App />`.
   render() {
     return (
       <div>
-        Todo App
+        {/*`<Form />` will hold your input field and your `Add Todo` and `Clear Completed` buttons. */}
+        <Form />
+        {/* `<Todo />` is a component that takes in the `todo` data and displays the task to the screen. */}
+        {/* Your input field should take in user input, and allow a user to press `Enter` or click on the `Submit Button` to add a todo to your list. */}
+        {/* Once a todo is submitted, the Todo List should re-render and show the added todo. */}
+        <Todo />
+        {/*`<TodoList />` receives your todos array and iterates over the list generating a new `<Todo />` for each element in the array. */}
+        <TodoList />
       </div>
-    )
+    );
   }
 }
